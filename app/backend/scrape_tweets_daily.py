4ad3f3d48e64b04f70a5699eaa49cf32fc37cf77
“mark-torres10” <markptorres1@gmail.com>
2021-02-09 12:54:19 -0500

    edit script to do automated tweet scrapes

--- app/backend/scrape_tweets_daily.py
+++ app/backend/scrape_tweets_daily.py
@@ -13,6 +13,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import numpy as np
 import pandas as pd
+import time
+import boto3
 
 
 def get_links_from_website(website, driver_path, username, password):
@@ -37,6 +39,108 @@ def get_links_from_website(website, driver_path, username, password):
     driver = webdriver.Chrome(driver_path)
     driver.get(website)
 
+    # click on button to log in
+    login_button = driver.find_element(
+        By.XPATH, "/html/body/div[3]/div/div[2]/nav/div/ul/li[2]/a")
+    login_button.click()
+
+    time.sleep(1)
+
+    # enter login info
+    username_field = driver.find_element(By.ID, "username")
+    username_field.clear()
+    password_field = driver.find_element(By.ID, "password")
+    password_field.clear()
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    driver.find_element(By.ID, "modalWindowRegisterSignInBtn").click()
+
+    # get links for datasets
+    has_link = True
+    idx = 1
+    filename_list = []
+    links_list = []
+
+    while has_link:
+
+        try:
+            row = driver.find_element(
+                By.XPATH, f"/html/body/div[5]/div/div/main/div/div[2]/div[1]/div/div/div/table[2]/tbody/tr[{idx}]/td[1]/span/a")
+            filename = row.text
+            link = row.get_attribute("href")
+
+            filename_list.append(filename)
+            links_list.append(link)
+            idx += 1
+
+        except Exception as e:
+            print("No more .csv links could be loaded")
+            print(e)
+            has_link = False
+
+    driver.close()
+
+    return (filename_list, links_list)
+
+
+def get_tweetIDs_to_hydrate(link):
+    """
+
+        Takes a link to a .csv file and returns both the original .csv file (with tweet ID + sentiment score) and list of tweet IDs
+
+        Args:
+            link (str): path to .csv file
+        Returns:
+            df: pandas df, with cols = ["tweet_id", "sentiment_score"] (sentiment score calculated by paper referenced in the 
+            IEEE link)
+            tweet_IDs: list (str), with the IDs for each tweet
+    """
+
+    df = pd.read_csv(link, names=["tweet_id", "sentiment_score"])
+
+    df.drop_duplicates(inplace=True)
+
+    tweet_IDs = list(df["tweet_id"])
+
+    return (df, tweet_IDs)
+
+
+def save_to_AWS(local_file, s3_file, AWS_bucket, AWS_access, AWS_secret):
+    """
+
+    Saves a local file to AWS
+
+    Args:
+        local_file: path (str) to a local file to export to AWS
+        s3_file: path (str) of file within the S3 bucket
+        AWS_bucket: name of S3 bucket
+        AWS_access: AWS access key
+        AWS_secret: AWS secret key
+
+    """
+
+    # connect boto3 wih AWS
+    try:
+        s3 = boto3.client("s3",
+                          aws_access_key_id=AWS_access,
+                          aws_secret_access_key=AWS_secret)
+    except Exception as e:
+        print("Connection with AWS unsuccessful")
+        print(e)
+        raise ValueError("Please fix connection error to AWS S3 bucket")
+
+    # upload data to AWS
+    try:
+        s3.upload_file(local_file, AWS_bucket, s3_file)
+    except Exception as e:
+        print("Error in uploading data to AWS")
+        print(e)
+        raise ValueError("Please fix error in uploading data to AWS")
+    finally:
+        return None
+
 
 if __name__ == "__main__":
 
@@ -44,7 +148,10 @@ if __name__ == "__main__":
 
     # IEEE_USERNAME
     # IEEE_PASSWORD
+    # AWS_ACCESS
+    # AWS_SECRET
 
+    DRIVER_PATH = "/Users/mark/Documents/research/gersteinLab/TextMining-master/chromedriver"
     # load tweets from website
     print("This part of the script loads the tweets from the website")
 
