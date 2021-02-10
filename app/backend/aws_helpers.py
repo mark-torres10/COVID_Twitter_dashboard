b173d74ddd9ff38c69afa03f1ee2d193a6a5f980
“mark-torres10” <markptorres1@gmail.com>
2021-02-09 22:55:36 -0500

    first draft of 'hydrate_tweets.py' complete

--- app/backend/aws_helpers.py
+++ app/backend/aws_helpers.py
@@ -42,3 +42,38 @@ def save_to_AWS(local_file, s3_file, AWS_resource, AWS_bucket, AWS_access, AWS_s
         raise ValueError("Please fix error in uploading data to AWS")
     finally:
         return None
+
+
+def load_from_AWS(local_file, s3_file, AWS_resource, AWS_bucket, AWS_access, AWS_secret):
+    """
+
+    Loads a file from an AWS resource (likely to be 's3')
+
+    Args:
+        local_file: path (str) to a local file to export to AWS
+        s3_file: path (str) of file within the S3 bucket
+        AWS_resource: the AWS resource used (e.g., "s3", "ec2", "dynamodb")
+        AWS_bucket: name of S3 bucket
+        AWS_access: AWS access key
+        AWS_secret: AWS secret key
+    """
+    # connect boto3 wih AWS
+    try:
+        s3 = boto3.client(AWS_resource,
+                          aws_access_key_id=AWS_access,
+                          aws_secret_access_key=AWS_secret)
+    except Exception as e:
+        print("Connection with AWS unsuccessful")
+        print(e)
+        raise ValueError("Please fix connection error to AWS resource")
+
+    try:
+        s3.download_file(Bucket=AWS_bucket, Key=s3_file, Filename=local_file)
+
+    except Exception as e:
+        print("Unable to download file from AWS to local storage")
+        print(e)
+        raise ValueError("Please fix error")
+
+    finally:
+        return None
--- app/backend/hydrate_tweets.py
+++ app/backend/hydrate_tweets.py
@@ -5,11 +5,19 @@
     This script takes tweet IDs and returns the full tweet JSON object associated with that tweet ID. 
 """
 import os
+import csv
+import pandas as pd
+import datetime
 import twarc
 from twarc import Twarc
+import aws_helpers
+from aws_helpers import save_to_AWS, load_from_AWS
 
 
 if __name__ == "__main__":
+
+    # change dir to this file's directory
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     # get env vars
     CONSUMER_KEY = os.environ["CONSUMER_KEY"]
@@ -17,9 +25,75 @@ if __name__ == "__main__":
     ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
     ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 
+    AWS_BUCKET = os.environ["AWS_BUCKET"]
+    AWS_ACCESS = os.environ["AWS_ACCESS"]
+    AWS_SECRET = os.environ["AWS_SECRET"]
+
+    # initialize paths
+    AWS_TWEET_DIR = "tweet_scrapes/"
+    ID_PATH = "./../../tweets/tweet_IDs/"
+    TWEETS_PATH = "./../../tweets/hydrated_tweets/"
+    LOCAL_TWEET_PATH = "./../../tweets/"
+
+    # local filenames of exports
+    ID_FILENAME = "scraped_tweet_IDs_2020-03-20_2021-02-09.csv"
+    TWEETS_FILENAME = "hydrated_tweets_2020-03-20_2021-02-09.csv"
+    LOCAL_EXPORT_ID_PATH = ID_PATH + ID_FILENAME
+    LOCAL_EXPORT_TWEETS_PATH = TWEETS_PATH + TWEETS_FILENAME
+    AWS_EXPORT_ID_PATH = AWS_TWEET_DIR + "tweet_IDs/" + ID_FILENAME
+    AWS_EXPORT_TWEETS_PATH = AWS_TWEET_DIR + "hydrated_tweets/" + TWEETS_FILENAME
+
+    try:
+        # load text file from AWS
+        load_from_AWS(LOCAL_EXPORT_ID_PATH,
+                      AWS_EXPORT_ID_PATH,
+                      "s3",
+                      AWS_BUCKET,
+                      AWS_ACCESS,
+                      AWS_SECRET)
+    except Exception as e:
+        print("Error in loading tweet IDs from AWS")
+        print(e)
+
+    # create tweet ID list
+    tweet_ID_list = []
+
+    with open(LOCAL_EXPORT_ID_PATH, "r") as f:
+        datareader = csv.reader(f)
+        for row in datareader:
+            tweet_ID_list.append(row)
+
     # connect twarc
     t = Twarc(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-    dir(t)
+    try:
+        # hydrate tweets
+        JSON_list = t.hydrate(tweet_ID_list)
+    except Exception as e:
+        print("Error in hydrating tweets")
+        print(e)
+        raise ValueError("Please resolve hydration issue.")
 
-    print(t)
+    # transform into pandas df
+    df = pd.read_json(JSON_list, lines=True)
+
+    # save locally
+    df.to_csv(LOCAL_EXPORT_TWEETS_PATH)
+
+    try:
+        # save to AWS
+        save_to_AWS(LOCAL_EXPORT_TWEETS_PATH,
+                    AWS_EXPORT_TWEETS_PATH,
+                    "s3",
+                    AWS_BUCKET,
+                    AWS_ACCESS,
+                    AWS_SECRET)
+    except Exception as e:
+        print("Error in exporting the local files to AWS")
+        print(e)
+        print("------------------")
+        raise ValueError(
+            "Please fix the error in exporting the local files to AWS")
+    finally:
+        print(
+            f"Finished with the execution of 'hydrate_tweets.py' at (in UTC time): {datetime.datetime.utcnow()}")
