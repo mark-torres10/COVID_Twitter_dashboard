ee6d09bf3233b85c365fccb83d0eb063dbe6e35c
“mark-torres10” <markptorres1@gmail.com>
2021-02-21 12:14:36 -0500

    updates to frontend

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'; 
+import Sidebar from "./Sidebar"; 
+import Header from "./Header"; 
 import './static/App.css'; 
 
 const App = () => {
@@ -6,10 +8,16 @@ const App = () => {
     <div className="app-div">
       <div className="app-flex-div">
         {/* Sidebar */}
+        {/*
         <div className="sidebar-div">Sidebar Div</div>
+        */}
+        <Sidebar/>
         {/* Header + Dashboard */}
         <div className="header-dash-flex-div">
+          <Header className="header-div"/>
+          {/*
           <div className="header-div">Header Div</div>
+          */}
           <div className="dashboard-div">Dashboard div</div>
         </div>
       </div>
